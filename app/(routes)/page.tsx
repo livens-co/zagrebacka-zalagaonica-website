bbcd874e5dc15c41949f7f1c6af72270d40a7bce
dcvijetovic <dominik@mintandlime.eu>
2023-10-18 22:20:03 +0200

    animations

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -18,9 +18,34 @@ import PaymentIcon from "@mui/icons-material/Payment";
 import VerifiedIcon from "@mui/icons-material/Verified";
 import HandshakeIcon from "@mui/icons-material/Handshake";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useInView } from "framer-motion";
 
 // export const revalidate = 3;
+
+type SectionProps = {
+  children: React.ReactNode;
+};
+
+function Section({ children }: SectionProps) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <div ref={ref}>
+      <div
+        style={{
+          transform: isInView ? "none" : "translateX(-100vw)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.7s ",
+        }}
+        // className="services-container"
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
 
 const HomePage = () => {
   // const products = await getProducts({ isFeatured: true });
@@ -53,158 +78,164 @@ const HomePage = () => {
       {/* HERO SECTION */}
 
       <div className="heroSection">
-        
         <video autoPlay muted loop src="/intro.mp4" playsInline>
           <source src="/intro.mp4" type="video/mp4" />
         </video>
-        <div className="videoOverlay"></div>
-        
-          <div className="heroImage">
-            <Image
-              priority
-              src="/assets/logo_white.png"
-              height={456}
-              width={1050}
-              alt="Zagrebačka zalagaonica"
-            />
-          </div>
-          <p>
-            Zagrebačka zalagaonica bavi se otkupom, zalogom i prodajom rabljene
-            robe poput zlata, luksuznih satova, elektronike, te ostalih predmeta
-            na upit.
-          </p>
-          <div className="contact">
-            <a href="tel:+385992173494" target="_blank">
-              <span>099 2173 494</span>
-              <div className="icon">
-                <LocalPhoneRoundedIcon />
-              </div>
-              <div className="buttonAnimation" />
-            </a>
-            <a href="mailto:info@zagrebacka-zalagaonica.hr" target="_blank">
-              <span>Kontaktirajte nas</span>
-              <div className="icon">
-                <MailRoundedIcon />
-              </div>
-              <div className="buttonAnimation" />
-            </a>
-            <a href="https://maps.app.goo.gl/zzsJvpo92Fsn8LscA" target="_blank">
-              <span>Krapinska ulica 5</span>
-              <div className="icon">
-                <FmdGoodRoundedIcon />
-              </div>
-              <div className="buttonAnimation" />
-            </a>
-          </div>
-      
-      
+        <div className="videoOverlay" />
+
+        <div className="heroImage">
+          <Image
+            priority
+            src="/assets/logo_white.png"
+            height={456}
+            width={1050}
+            alt="Zagrebačka zalagaonica"
+          />
+        </div>
+        <p>
+          Zagrebačka zalagaonica bavi se otkupom, zalogom i prodajom rabljene
+          robe poput zlata, luksuznih satova, elektronike, te ostalih predmeta
+          na upit.
+        </p>
+        <div className="contact">
+          <a href="tel:+385992173494" target="_blank">
+            <span>099 2173 494</span>
+            <div className="icon">
+              <LocalPhoneRoundedIcon />
+            </div>
+            <div className="buttonAnimation" />
+          </a>
+          <a href="mailto:info@zagrebacka-zalagaonica.hr" target="_blank">
+            <span>Kontaktirajte nas</span>
+            <div className="icon">
+              <MailRoundedIcon />
+            </div>
+            <div className="buttonAnimation" />
+          </a>
+          <a href="https://maps.app.goo.gl/zzsJvpo92Fsn8LscA" target="_blank">
+            <span>Krapinska ulica 5</span>
+            <div className="icon">
+              <FmdGoodRoundedIcon />
+            </div>
+            <div className="buttonAnimation" />
+          </a>
+        </div>
       </div>
 
       {/* OTKUP ZALOG PRODAJA */}
       {/* <Container> */}
-      <div className="servicesSection">
-        <div className="service">
-          <div className="serviceImage">
-            <Image
-              fill
-              src="https://www.notebookcheck.biz/uploads/tx_nbc2/AppleiPhone14Pro__1__01.JPG"
-              alt="Otkup"
-            />
-            <div className="overlay" />
+      <Section>
+        <div className="servicesSection">
+          <div className="service">
+            <div className="serviceImage">
+              <Image
+                fill
+                src="https://www.notebookcheck.biz/uploads/tx_nbc2/AppleiPhone14Pro__1__01.JPG"
+                alt="Otkup"
+              />
+              <div className="overlay" />
+            </div>
+            <Link href="/otkup" className="serviceTitle">
+              Otkup <ArrowForwardIcon />
+            </Link>
           </div>
-          <Link href="/otkup" className="serviceTitle">
-            Otkup <ArrowForwardIcon />
-          </Link>
-        </div>
-        <div className="service">
-          <div className="serviceImage">
-            <Image
-              fill
-              src="https://img.freepik.com/premium-photo/3d-rendering-shiny-gold-bars-stacked-black-background-with-copy-space-3-illustration_67155-13973.jpg"
-              alt="Zalog"
-            />
-            <div className="overlay" />
+          <div className="service">
+            <div className="serviceImage">
+              <Image
+                fill
+                src="https://img.freepik.com/premium-photo/3d-rendering-shiny-gold-bars-stacked-black-background-with-copy-space-3-illustration_67155-13973.jpg"
+                alt="Zalog"
+              />
+              <div className="overlay" />
+            </div>
+            <Link href="/zalog" className="serviceTitle">
+              Zalog <ArrowForwardIcon />
+            </Link>
           </div>
-          <Link href="/zalog" className="serviceTitle">
-            Zalog <ArrowForwardIcon />
-          </Link>
-        </div>
-        <div className="service">
-          <div className="serviceImage">
-            <Image
-              fill
-              src="https://lidermedia.hr/images/slike/2022/01/04/o_393279_1024.jpg"
-              alt="Otkup"
-            />
-            <div className="overlay" />
+          <div className="service">
+            <div className="serviceImage">
+              <Image
+                fill
+                src="https://lidermedia.hr/images/slike/2022/01/04/o_393279_1024.jpg"
+                alt="Otkup"
+              />
+              <div className="overlay" />
+            </div>
+            <Link href="/ponuda" className="serviceTitle">
+              Prodaja <ArrowForwardIcon />
+            </Link>
           </div>
-          <Link href="/ponuda" className="serviceTitle">
-            Prodaja <ArrowForwardIcon />
-          </Link>
         </div>
-      </div>
+      {/* </Section> */}
       {/* </Container> */}
       {/* O NAMA */}
-
-      <div className="aboutUsSection">
-        <div className="title">
-          <h1>O nama</h1>
+      {/* <Section> */}
+        <div className="aboutUsSection">
+          <div className="title">
+            <h1>O nama</h1>
+          </div>
+          <div className="text">
+            <p>
+              Zagrebačka zalagaonica bavi se otkupom, zalogom i prodajom
+              rabljene robe poput zlata, luksuznih satova, elektronike,
+              nekretnina, automobila te ostalih predmeta na upit.
+            </p>
+            <p>
+              Ako želite prodati svoje predmete, nudimo vam povoljne opcije
+              prodaje. Naša zalagaonica ima široku mrežu kupaca i stručno znanje
+              koje nam pomaže postići najbolje cijene za vaše predmete.
+            </p>
+            <p>
+              Ako trebate hitan kratkoročni zajam, naša usluga zaloga je idealna
+              za vas. Možete koristiti svoje predmete kao kolateral kako biste
+              dobili novčani zajam, a mi ćemo se brinuti o sigurnom čuvanju
+              vaših dragocjenosti dok ne otplatite zajam.
+            </p>
+          </div>
         </div>
-        <div className="text">
-          <p>
-            Zagrebačka zalagaonica bavi se otkupom, zalogom i prodajom rabljene
-            robe poput zlata, luksuznih satova, elektronike, nekretnina,
-            automobila te ostalih predmeta na upit.
-          </p>
-          <p>
-            Ako želite prodati svoje predmete, nudimo vam povoljne opcije
-            prodaje. Naša zalagaonica ima široku mrežu kupaca i stručno znanje
-            koje nam pomaže postići najbolje cijene za vaše predmete.
-          </p>
-          <p>
-            Ako trebate hitan kratkoročni zajam, naša usluga zaloga je idealna
-            za vas. Možete koristiti svoje predmete kao kolateral kako biste
-            dobili novčani zajam, a mi ćemo se brinuti o sigurnom čuvanju vaših
-            dragocjenosti dok ne otplatite zajam.
-          </p>
-        </div>
-      </div>
-
+      </Section>
       {/* IZDVOJENI PROIZVODI */}
-
-      <div className="featuredProducts">
-        <h1>Izdvojeni proizvodi</h1>
-        {products && <ProductList title="" items={products} />}
-      </div>
-
+      <Section>
+        <div className="featuredProducts">
+          <h1>Izdvojeni proizvodi</h1>
+          {products && <ProductList title="" items={products} />}
+        </div>
+      </Section>
       {/* SLOGAN */}
-      <div className="slogan">
-        <h1>
-          Zagrebačka zalagaonica - vaš put prema brzim financijskim rješenjima.
-        </h1>
-      </div>
+      <Section>
+        <div className="slogan">
+          <h1>
+            Zagrebačka zalagaonica - vaš put prema brzim financijskim
+            rješenjima.
+          </h1>
+        </div>
+      {/* </Section> */}
 
       {/* NOVOSTI */}
-      <div className="featuredArticles">
-        <h1 className="articlesTitle">Novosti</h1>
-        {articles && <ArticleList items={articles} />}
-      </div>
+      {/* <Section> */}
+        <div className="featuredArticles">
+          <h1 className="articlesTitle">Novosti</h1>
+          {articles && <ArticleList items={articles} />}
+        </div>
+      </Section>
 
       {/* GRID NACINI PLACANJA / PROVJERENI PROIZVODI  / ISKUSTVO I POVJERENJE */}
-      <div className="paymentMethodsSection">
-        <div className="gridItem">
-          <PaymentIcon />
-          <p>Mogućnost plaćanja karticama na rate i gotovinom</p>
+      <Section>
+        <div className="paymentMethodsSection">
+          <div className="gridItem">
+            <PaymentIcon />
+            <p>Mogućnost plaćanja karticama na rate i gotovinom</p>
+          </div>
+          <div className="gridItem">
+            <VerifiedIcon />
+            <p>Provjereni proizvodi</p>
+          </div>
+          <div className="gridItem">
+            <HandshakeIcon />
+            <p>Iskustvo i povjerenje</p>
+          </div>
         </div>
-        <div className="gridItem">
-          <VerifiedIcon />
-          <p>Provjereni proizvodi</p>
-        </div>
-        <div className="gridItem">
-          <HandshakeIcon />
-          <p>Iskustvo i povjerenje</p>
-        </div>
-      </div>
+      </Section>
     </div>
   );
 };
